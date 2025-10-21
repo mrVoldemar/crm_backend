4079da5cbe6af6625f4d77da74b56c86f9cfbedf
vladimir <v447-247-172@yandex.ru>
2025-10-21 14:54:07 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/mrVoldemar/crm_backend/internal/app"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	a, err := app.NewApp(ctx)
+	if err != nil {
+		log.Fatalf("failed to create app: %v", err)
+	}
+
+	err = a.Run()
+	if err != nil {
+		log.Fatalf("failed to run app: %v", err)
+	}
+
+}
---
+++ internal/app/service_provider.go
@@ -0,0 +1,103 @@
+package app
+
+import (
+	"context"
+	"log"
+
+	"github.com/vp-go-micro/go-auth/internal/client/db"
+	"github.com/vp-go-micro/go-auth/internal/client/db/pg"
+	"github.com/vp-go-micro/go-auth/internal/client/db/transaction"
+
+	"github.com/vp-go-micro/go-auth/internal/api/user"
+	"github.com/vp-go-micro/go-auth/internal/config"
+	"github.com/vp-go-micro/go-auth/internal/config/env"
+	"github.com/vp-go-micro/go-auth/internal/repository"
+	userRepository "github.com/vp-go-micro/go-auth/internal/repository/user"
+	"github.com/vp-go-micro/go-auth/internal/service"
+	userService "github.com/vp-go-micro/go-auth/internal/service/user"
+)
+
+type serviceProvider struct {
+	pgConfig   config.PGConfig
+	grpcConfig config.GRPCConfig
+
+	dbClient       db.Client
+	txManager      db.TxManager
+	userRepository repository.UserRepository
+
+	userService service.UserService
+
+	userImpl *user.Implementation
+}
+
+func newServiceProvider() *serviceProvider {
+	return &serviceProvider{}
+}
+
+func (s *serviceProvider) PGConfig() config.PGConfig {
+	if s.pgConfig == nil {
+		pgConfig, err := env.NewPGConfig()
+		if err != nil {
+			log.Fatalf("failed to get pg config: %s", err.Error())
+		}
+		s.pgConfig = pgConfig
+	}
+	return s.pgConfig
+}
+
+func (s *serviceProvider) GRPCConfig() config.GRPCConfig {
+	if s.grpcConfig == nil {
+		grpcConfig, err := env.NewGRPCConfig()
+		if err != nil {
+			log.Fatalf("failed to get grpc config: %s", err.Error())
+		}
+		s.grpcConfig = grpcConfig
+	}
+	return s.grpcConfig
+}
+
+func (s *serviceProvider) DBClient(ctx context.Context) db.Client {
+	if s.dbClient == nil {
+		cl, err := pg.New(ctx, s.PGConfig().DSN())
+		if err != nil {
+			log.Fatalf("failed to connect to database: %v", err)
+		}
+
+		err = cl.DB().Ping(ctx)
+		if err != nil {
+			log.Fatalf("failed to ping database: %v", err)
+		}
+
+		s.dbClient = cl
+	}
+	return s.dbClient
+}
+
+func (s *serviceProvider) TxManager(ctx context.Context) db.TxManager {
+	if s.txManager == nil {
+		s.txManager = transaction.NewTransactionManager(s.DBClient(ctx).DB())
+	}
+
+	return s.txManager
+}
+
+func (s *serviceProvider) UserRepository(ctx context.Context) repository.UserRepository {
+	if s.userRepository == nil {
+		s.userRepository = userRepository.NewRepository(s.DBClient(ctx))
+	}
+	return s.userRepository
+}
+
+func (s *serviceProvider) UserService(ctx context.Context) service.UserService {
+	if s.userService == nil {
+		s.userService = userService.NewService(s.UserRepository(ctx), s.TxManager(ctx))
+	}
+	return s.userService
+}
+
+func (s *serviceProvider) UserImpl(ctx context.Context) *user.Implementation {
+	if s.userImpl == nil {
+		s.userImpl = user.NewImplementation(s.UserService(ctx))
+	}
+	return s.userImpl
+}
