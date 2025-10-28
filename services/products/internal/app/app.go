0a7507769eddad068a3bf20df348908324352e2d
vladimir <v447-247-172@yandex.ru>
2025-10-28 18:13:18 +0300

    service products init

---
+++ services/products/internal/app/app.go
@@ -0,0 +1,101 @@
+package app
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+
+	"github.com/mrVoldemar/crm_backend/services/products/internal/closer"
+	"github.com/mrVoldemar/crm_backend/services/products/internal/config"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type App struct {
+	serviceProvider *serviceProvider
+	grpcServer      *grpc.Server
+}
+
+func NewApp(ctx context.Context) (*App, error) {
+	a := &App{}
+
+	err := a.initDeps(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
+
+func (a *App) Run() error {
+	defer func() {
+		closer.CloseAll()
+		closer.Wait()
+	}()
+	return a.runGRPCServer()
+}
+
+func (a *App) initDeps(ctx context.Context) error {
+	inits := []func(ctx context.Context) error{
+		a.initConfig,
+		a.initServiceProvider,
+		a.initGRPCServer,
+	}
+	for _, f := range inits {
+		err := f(ctx)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
+}
+
+func (a *App) initConfig(_ context.Context) error {
+	flag.Parse()
+
+	err := config.Load(configPath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) initServiceProvider(_ context.Context) error {
+	a.serviceProvider = newServiceProvider()
+	return nil
+}
+
+func (a *App) initGRPCServer(ctx context.Context) error {
+	a.grpcServer = grpc.NewServer()
+
+	reflection.Register(a.grpcServer)
+
+	//userDesc.RegisterUserV1Server(a.grpcServer, a.serviceProvider.UserImpl(ctx))
+
+	return nil
+}
+
+func (a *App) runGRPCServer() error {
+	log.Printf("GRPC server is running on %s", a.serviceProvider.GRPCConfig().GRPCAddress())
+
+	list, err := net.Listen("tcp", a.serviceProvider.GRPCConfig().GRPCAddress())
+	if err != nil {
+		return err
+	}
+
+	err = a.grpcServer.Serve(list)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ services/products/internal/client/db/transaction/transaction.go
@@ -0,0 +1,89 @@
+package transaction
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/mrVoldemar/crm_backend/services/products/internal/client/db"
+	"github.com/mrVoldemar/crm_backend/services/products/internal/client/db/pg"
+	"github.com/pkg/errors"
+)
+
+type manager struct {
+	db db.Transactor
+}
+
+// NewTransactionManager создает новый менеджер транзакций, который удовлетворяет интерфейсу db.TxManager
+func NewTransactionManager(db db.Transactor) db.TxManager {
+	return &manager{
+		db: db,
+	}
+}
+func NewTransactionManagerMock(deps ...interface{}) db.TxManager {
+	// Return a minimal mock implementation
+	return &mockTxManager{}
+}
+
+type mockTxManager struct{}
+
+func (m *mockTxManager) ReadCommitted(ctx context.Context, f db.Handler) error {
+	// Simply execute the handler without actual transaction management
+	return f(ctx)
+}
+
+// transaction основная функция, которая выполняет указанный пользователем обработчик в транзакции
+func (m *manager) transaction(ctx context.Context, opts pgx.TxOptions, fn db.Handler) (err error) {
+	// Если это вложенная транзакция, пропускаем инициацию новой транзакции и выполняем обработчик.
+	tx, ok := ctx.Value(pg.TxKey).(pgx.Tx)
+	if ok {
+		return fn(ctx)
+	}
+
+	// Стартуем новую транзакцию.
+	tx, err = m.db.BeginTx(ctx, opts)
+	if err != nil {
+		return errors.Wrap(err, "can't begin transaction")
+	}
+
+	// Кладем транзакцию в контекст.
+	ctx = pg.MakeContextTx(ctx, tx)
+
+	// Настраиваем функцию отсрочки для отката или коммита транзакции.
+	defer func() {
+		// восстанавливаемся после паники
+		if r := recover(); r != nil {
+			err = errors.Errorf("panic recovered: %v", r)
+		}
+
+		// откатываем транзакцию, если произошла ошибка
+		if err != nil {
+			if errRollback := tx.Rollback(ctx); errRollback != nil {
+				err = errors.Wrapf(err, "errRollback: %v", errRollback)
+			}
+
+			return
+		}
+
+		// если ошибок не было, коммитим транзакцию
+		if nil == err {
+			err = tx.Commit(ctx)
+			if err != nil {
+				err = errors.Wrap(err, "tx commit failed")
+			}
+		}
+	}()
+
+	// Выполните код внутри транзакции.
+	// Если функция терпит неудачу, возвращаем ошибку, и функция отсрочки выполняет откат
+	// или в противном случае транзакция коммитится.
+	if err = fn(ctx); err != nil {
+		err = errors.Wrap(err, "failed executing code inside transaction")
+	}
+
+	return err
+}
+
+func (m *manager) ReadCommitted(ctx context.Context, f db.Handler) error {
+	txOpts := pgx.TxOptions{IsoLevel: pgx.ReadCommitted}
+	return m.transaction(ctx, txOpts, f)
+}
